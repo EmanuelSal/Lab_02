21b6c60e8ee6f3388ed3a491415fa4577043a0c9
evlima <limaemanuel06@gmail.com>
2022-05-30 23:18:13 -0300

    Update Disciplina.java

--- Laboratorio_2/src/Disciplina.java
+++ Laboratorio_2/src/Disciplina.java
@@ -1,1 +1,29 @@
+package LAB2_EMANUELVINICIUS;
 
+public class Descanso {
+	
+	private int horas;
+	private int semanas;
+	private int status;
+	
+	public void defineHorasDescanso(int valor) {
+		this.horas = valor;
+	}
+	
+	public void defineNumeroSemanas(int valor) {
+		this.semanas = valor;
+		if (this.semanas != 0) {
+			status = this.horas / this.semanas;
+		}
+	}
+	
+	public String getStatusGeral() {
+		if (this.horas == 0 || this.semanas == 0) {
+			return "cansado";
+		} else if (status >= 26) {
+			return "descansado";
+		} else {
+			return "cansado";
+		}
+	}
+}	
