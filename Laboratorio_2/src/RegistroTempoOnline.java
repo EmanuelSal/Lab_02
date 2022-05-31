d2bafae8677e39e7c67517c402a1bf54e22173e0
evlima <limaemanuel06@gmail.com>
2022-05-30 21:19:54 -0300

    Update RegistroTempoOnline.java

--- Laboratorio_2/src/RegistroTempoOnline.java
+++ Laboratorio_2/src/RegistroTempoOnline.java
@@ -1,4 +1,32 @@
+package LAB2_EMANUELVINICIUS;
 
 public class RegistroTempoOnline {
-
+	
+	private String disciplina;
+	private int tempoOnlineEsperado = 120;
+	private int tempoAtual;
+	
+	
+	public RegistroTempoOnline(String nomeDisciplina) {
+		this.disciplina = nomeDisciplina;
+	}
+	
+	public RegistroTempoOnline (String nomeDisciplina, int tempoOnlineEsperado) {
+		this.disciplina = nomeDisciplina;
+		this.tempoOnlineEsperado = tempoOnlineEsperado;
+	}
+	
+	public void adicionaTempoOnline(int tempo) {
+		this.tempoAtual += tempo;
+	}
+	
+	public boolean atingiuMetaTempoOnline() {
+		if (this.tempoAtual >= tempoOnlineEsperado) {
+			return true;
+		} 
+		 return false;
+	}
+	public String toString() {
+		return disciplina + " " + tempoAtual + "/" + tempoOnlineEsperado;
+	}
 }
