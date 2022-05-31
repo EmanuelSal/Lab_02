fcc4493542276129f56b317c0017dfdc5abf44d3
evlima <limaemanuel06@gmail.com>
2022-05-30 21:19:27 -0300

    Update Descanso.java

--- Laboratorio_2/src/Descanso.java
+++ Laboratorio_2/src/Descanso.java
@@ -1,4 +1,26 @@
+package LAB2_EMANUELVINICIUS;
 
 public class Descanso {
-
-}
+	
+	private int horas;
+	private int semanas;
+	private int status = this.horas / this.semanas;
+	
+	public void defineHorasDescanso(int valor) {
+		this.horas = valor;
+	}
+	
+	public void defineNumeroSemanas(int valor) {
+		this.semanas = valor;
+	}
+	
+	public String getStatusGeral() {
+		if (this.horas == 0 || this.semanas == 0) {
+			return "cansado";
+		} else if (status < 26) {
+			return "cansado";
+		} else {
+			return "descansado";
+		}
+	}
+}	
